81e763b829cfda9cf138072290876bfb1ebfcab6
nikolaka <nikolaka@gmail.com>
2014-08-28 17:16:23 +0300

    testing invalid emaila/names & duplicate emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ActiveRecord::Base
 
-	has_secure_password
+	#has_secure_password
 	
 	has_many :holdings
 	has_many :utransactions
 	has_many :contracts, :through => :holdings
 	has_many :contracts, :through => :utransactions
 
-	validates :name, presence: true, :length => { :within => 8..25 },
-                       :uniqueness => true
+	validates :name, presence: true, :length => { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
 
     scope :sorted, lambda { order("users.rank ASC")}
 end
