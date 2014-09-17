38f9ebb2bce2cb2a41e051b7e8b6c0fb6a784231
mpthimios <mpthimios@gmail.com>
2014-09-17 11:51:48 +0300

    minor error correction.

--- db/migrate/20140826145624_create_contracts.rb
+++ db/migrate/20140826145624_create_contracts.rb
@@ -8,8 +8,8 @@ class CreateContracts < ActiveRecord::Migration
       t.decimal :opening_price, 	precision: 8,	scale: 2
       t.datetime :opening_date
       t.datetime :end_date
-      t.float :total_shares, default => 0
-      t.float :total_amount_wagered, default => 0
+      t.float :total_shares, default:  0
+      t.float :total_amount_wagered, default:  0
       t.integer :volume_traded
       t.boolean :status,			default: "0"
       t.integer :position
