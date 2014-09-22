913415291dfa806002fc6277397a01b7495776ae
nikolaka <nikolaka@gmail.com>
2014-09-22 18:40:16 +0300

    delete default categories from markets

--- app/models/market.rb
+++ app/models/market.rb
@@ -4,12 +4,12 @@ class Market < ActiveRecord::Base
 	# you need to add a position column to desired table:
 	# acts_as_list
 
-	CATEGORY_TYPES = ['Economics', 'Politics', 'Environment']
+	#CATEGORY_TYPES = ['Economics', 'Politics', 'Environment']
 
 	validates_presence_of :name, :description, :published_date, :arbitration_date
 	validates_length_of :name, :maximum => 255
 	validates_uniqueness_of :name
-	validates_inclusion_of :category, :in => CATEGORY_TYPES, :message => "must be one of: #{CATEGORY_TYPES.join(',')}"
+	#validates_inclusion_of :category, :in => CATEGORY_TYPES, :message => "must be one of: #{CATEGORY_TYPES.join(',')}"
 	validates_presence_of :category
 
   scope :sorted, lambda { order("markets.id ASC")}
