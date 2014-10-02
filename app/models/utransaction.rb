f1562f52a1588896bd62454fbbdb26ba9ac6a5e9
nikolaka <nikolaka@gmail.com>
2014-10-02 10:17:36 +0300

    update my_orders page

--- app/models/utransaction.rb
+++ app/models/utransaction.rb
@@ -20,7 +20,7 @@ class Utransaction < ActiveRecord::Base
     self.user.investment_amount += (1) * cost
     self.user.save
     logger.debug "the transaction cost is: " + cost.to_s
-    Holding.find_or_initialize_by(user_id: self.user_id, contract_id: self.contract_id, price_purchased: contract_current_value) \
+    Holding.find_or_initialize_by(user_id: self.user_id, contract_id: self.contract_id) \
       .update_attributes(self)
   end
 
