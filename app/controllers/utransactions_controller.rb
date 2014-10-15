b006454a0015b4382025192d0593ca88b7f40806
nikolaka <nikolaka@gmail.com>
2014-10-15 18:43:03 +0300

    calculate amount spent/gained in trading history page

--- app/controllers/utransactions_controller.rb
+++ app/controllers/utransactions_controller.rb
@@ -4,6 +4,7 @@ class UtransactionsController < ApplicationController
   def index
     @utransactions = current_user.utransactions.all
     @utransactions = current_user.utransactions.order("created_at DESC").paginate(page: params[:page], :per_page => 10)
+    @holdings = current_user.holdings.all
   end
 
 
