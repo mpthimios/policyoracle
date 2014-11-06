4b1c46b2298c79ad7a25cd32060915f9fe5bc9c2
mpthimios <mpthimios@gmail.com>
2014-11-06 15:48:07 +0200

    updated graph

--- app/assets/javascripts/graph.js
+++ app/assets/javascripts/graph.js
@@ -2,7 +2,7 @@
  * Created by efthimios on 10/10/2014.
  */
 //data format
-var data = {
+var demo_data = {
     prices:
         [
             {
@@ -24,17 +24,18 @@ var data = {
 
 //some variables
 var available_size = 960,
-    margin = {top: 0.05*available_size, right: 0.01*available_size, bottom: 0.1*available_size, left: 0.05*available_size},
-    margin2 = {top: 0.45*available_size, right: 0.01*available_size, bottom: 0.02*available_size, left: 0.05*available_size},
-    width = available_size - margin.left - margin.right,
-    height = 0.5*available_size - margin.top - margin.bottom,
-    height2 = 0.5*available_size - margin2.top - margin2.bottom,
-barHeight = 0.09*available_size,
-bar2Height = 0.03*available_size,
-barWidth = 0.01*available_size,
-bar2Width = 0.005*available_size,
-color = d3.scale.category10(),
-svg, legend, focus, context, tooltip, div;
+    margin = {top: 40, right: 40, bottom: 100, left: 40},
+    margin2 = {top: 420, right: 20, bottom: 20, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    lineChartHeight = 400 - margin.top - margin.bottom,
+    height2 = 467 - margin2.top - margin2.bottom;
+line2ChartHeight = 400 - margin2.top - margin2.bottom,
+    barHeight = 80;
+bar2Height = 30;
+barWidth = 10;
+bar2Width = 5;
+color = d3.scale.category10();
 
 //date format
 var parseDate = d3.time.format("%Y-%m-%d %H:%M").parse;
@@ -43,7 +44,7 @@ var formatDate = d3.time.format("%Y-%m-%d %H:%M");
 //scales
 var x = d3.time.scale().range([0, width]),
     x2 = d3.time.scale().range([0, width]),
-    y = d3.scale.linear().range([height, 0]),
+    y = d3.scale.linear().range([lineChartHeight, 0]),
     y2 = d3.scale.linear().range([height2, 0]);
 barY = d3.scale.linear().range([barHeight, 0]);
 bar2Y = d3.scale.linear().range([bar2Height, 0]);
@@ -51,7 +52,8 @@ bar2Y = d3.scale.linear().range([bar2Height, 0]);
 //axis
 var xAxis = d3.svg.axis().scale(x).orient("bottom"),
     xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
-    yAxis = d3.svg.axis().scale(y).orient("left");
+    yAxis = d3.svg.axis().scale(y).orient("left").ticks(7);;
+    baryAxis = d3.svg.axis().scale(barY).orient("left").ticks(3);
 
 //need one for volume?
 
@@ -59,17 +61,38 @@ var brush = d3.svg.brush()
     .x(x2)
     .on("brush", brushed);
 
-var line = d3.svg.area()
+var line = d3.svg.line()
     .interpolate("linear")
     .x(function(d) { return x(d.date); })
-    .y0(height)
-    .y1(function(d) { return y(d.price); });
+    .y(function(d) { return y(d.price); });
 
-var line2 = d3.svg.area()
+var line2 = d3.svg.line()
     .interpolate("linear")
     .x(function(d) { return x2(d.date); })
-    .y0(height2)
-    .y1(function(d) { return y2(d.price); });
+    .y(function(d) { return y2(d.price); });
+
+var svg = d3.select("chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom -40);
+
+svg.append("defs").append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", width)
+    .attr("height", lineChartHeight);
+
+var priceText = svg.append("g")
+    .append("text")
+    .text("Price")
+    .style("color", "black")
+    .style("font-size", "12px")
+    .attr("transform", "translate(" + margin.left + ", " + (margin.top -4) +")");
+
+var volumeText = svg.append("g")
+    .append("text")
+    .text("Volume")
+    .style("font-size", "12px")
+    .attr("transform", "translate(" + margin.left + ", " + (lineChartHeight + margin.top +38) +")");
 
 function make_y_axis() {
     return d3.svg.axis()
@@ -85,13 +108,27 @@ function make_x_axis() {
         .ticks(4);
 }
 
+function make_bar_x_axis() {
+    return d3.svg.axis()
+        .scale(barx)
+        .orient("bottom")
+        .ticks(4);
+}
+
+function make_bar_y_axis() {
+    return d3.svg.axis()
+        .scale(barY)
+        .orient("left")
+        .ticks(4);
+}
+
 function chart(data) {
     console.log(data)
     color.domain(data.prices.map(function(entry) {return entry.key}));
 
     svg = d3.select("chart").append("svg")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
+        .attr("height", height + margin.top + margin.bottom - 40);
 
     svg.append("defs").append("clipPath")
         .attr("id", "clip")
@@ -178,7 +215,7 @@ function chart(data) {
     focus.append("rect")
         .attr("x", 0)
         .attr("y", 0)
-        .attr("height", (height))
+        .attr("height", (lineChartHeight))
         .attr("width", (width - 1 ))
         .style("stroke", "black")
         .style("fill", "none")
@@ -190,7 +227,7 @@ function chart(data) {
         .attr("x", 0)
         .attr("y", 0)
         .attr("width", width)
-        .attr("height", height);
+        .attr("height", lineChartHeight);
 
     focus.append("g")
         .attr("class", "grid")
@@ -201,16 +238,40 @@ function chart(data) {
 
     focus.append("g")
         .attr("class", "grid")
-        .attr("transform", "translate(0," + (height+margin.top) + ")")
+        .attr("transform", "translate(0," + (lineChartHeight) + ")")
         .call(make_x_axis()
-            .tickSize(-(height + margin.top), 0, 0)
+            .tickSize(-(lineChartHeight), 0, 0)
+            .tickFormat("")
+    );
+
+    focus.append("rect")
+        .attr("x", 0)
+        .attr("y", lineChartHeight + margin.top)
+        .attr("height", (barHeight-20))
+        .attr("width", (width))
+        .style("stroke", "#E0E0E0")
+        .style("fill", "#E0E0E0")
+        .style("stroke-width", "0.5");
+    focus.append("clipPath")
+        .attr("id", "barFocusClip")
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", lineChartHeight + margin.top)
+        .attr("width", width)
+        .attr("height", barHeight-20);
+
+    focus.append("g")
+        .attr("class", "grid")
+        .attr("transform", "translate(0," + (lineChartHeight+margin.top) + ")")
+        .call(make_bar_y_axis()
+            .tickSize(-width, 0, 0)
             .tickFormat("")
     );
 
     bar = focus.selectAll(".bars")
         .data(volumes)
         .enter().append("g")
-        .attr('clip-path', 'url(#focusClip)')
+        .attr('clip-path', 'url(#barFocusClip)')
 
     bar.append("rect")
         .attr("class", "bar")
@@ -282,13 +343,24 @@ function chart(data) {
 
     focus.append("g")
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", "translate(0," + lineChartHeight + ")")
         .call(xAxis);
 
     focus.append("g")
         .attr("class", "y axis")
         .call(yAxis);
 
+    focus.append("g")
+        .attr("class", "y axis")
+        .attr("transform", "translate(0, "+(lineChartHeight+margin.top) +")")
+        .call(baryAxis);
+
+    context.append("rect")
+        .attr("height", bar2Height)
+        .attr("y", height2 - bar2Height)
+        .attr("width", width)
+        .style("stroke", "white")
+        .style("fill", "white");
 
     contextbar = context.selectAll(".bars")
         .data(volumes)
@@ -317,10 +389,12 @@ function chart(data) {
             return 'tag2'+d.key.replace(/\s+/g, ''); })
         .attr("d", function(d) { return line2(d.prices); })
 
+    /*
     context.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + height2 + ")")
         .call(xAxis2);
+    */
 
     context.append("g")
         .attr("class", "x brush")
