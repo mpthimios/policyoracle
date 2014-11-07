7dfe6a3298881ead06616d063f76b0892f2653a2
nikolaka <nikolaka@gmail.com>
2014-11-07 15:32:43 +0200

    changes in env/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,19 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'policyoracle.com'
+  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'policyoracle.com',
+    user_name:            'policyoracle',
+    password:             'p0l1cYor@cl3',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
