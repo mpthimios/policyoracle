cab22599f40d4c0ddb48e4ed5be90061ffa256a9
Niki Nikolakakou <nikolaka@niki-imu>
2015-02-27 16:17:06 +0200

    revised market#show page | one page with scroll tabs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -133,4 +133,19 @@ $(document).ready(function() {
         // from throwing a 'preventDefault' error due to us overriding the anchor usage.
         return false;
     });
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
 });
