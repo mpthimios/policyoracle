7e2c30c3ddcf5da9c93212ebfe66a829dc6eaae6
Niki Nikolakakou <nikolaka@niki-imu>
2015-02-27 11:41:31 +0200

    after close the market redirect_to the same market

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -75,8 +75,8 @@ class ContractsController < ApplicationController
 
     @winning_contract = Contract.find_by_id(params["correct_id"]).close
     flash[:notice] = "Market was successfully closed."
-    
-    redirect_to markets_path(@market)
+    contract =  Contract.find_by_id(params["correct_id"])
+    redirect_to market_path(contract.market)
   end
 
   private
